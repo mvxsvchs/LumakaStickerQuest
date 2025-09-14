43d898fa43127462fd8889ab8632ee695a4bab3b
Maximilian Sachs <maximilian.lukas.sachs@outlook.de>
2025-09-14 20:01:13 +0200

    fixed navbar icon size

--- app/src/main/java/com/example/lumaka/ui/component/NavBar.kt
+++ app/src/main/java/com/example/lumaka/ui/component/NavBar.kt
@@ -1,16 +1,22 @@
 package com.example.lumaka.ui.component
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier // Added import
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp // Added import
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 
@@ -18,11 +24,10 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 sealed class NavigationItem(
     val route: String,
     val icon: ImageVector,
-    val title: String
 ) {
-    object Home : NavigationItem("home", Icons.Filled.Home, "Home")
-    object Profile : NavigationItem("profile", Icons.Filled.Person, "Profile")
-    object Settings : NavigationItem("settings", Icons.Filled.Settings, "Settings")
+    object Home : NavigationItem("home", Icons.Filled.Home)
+    object Profile : NavigationItem("profile", Icons.Filled.Person,)
+    object Settings : NavigationItem("settings", Icons.Filled.Settings,)
 }
 
 val items = listOf(
@@ -33,14 +38,21 @@ val items = listOf(
 
 @Composable
 fun NavigationBar(navController: NavController) {
-    NavigationBar {
+    NavigationBar(
+        containerColor = MaterialTheme.colorScheme.surface,
+    ) {
         val navBackStackEntry by navController.currentBackStackEntryAsState()
         val currentRoute = navBackStackEntry?.destination?.route
 
         items.forEach { item ->
             NavigationBarItem(
-                icon = { Icon(imageVector = item.icon, contentDescription = item.title) },
-                label = { Text(text = item.title) },
+                icon = { 
+                    Icon(
+                        imageVector = item.icon,
+                        contentDescription = "",
+                        modifier = Modifier.size(36.dp)
+                    ) 
+                },
                 selected = currentRoute == item.route,
                 onClick = { navController.navigate(item.route) },
             )
