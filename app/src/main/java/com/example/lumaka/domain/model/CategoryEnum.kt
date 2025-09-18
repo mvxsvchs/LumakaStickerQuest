37467ad08f3402523b16707985cd6e7d3ba01770
Maximilian Sachs <maximilian.lukas.sachs@outlook.de>
2025-09-18 10:15:36 +0200

    Renamed category and updated string resources
    The category "selfcare" was added.
    The order of categories was changed.
    The string resource "home.button.add" was removed.
    The string resource "home.title" was updated.

--- app/src/main/java/com/example/lumaka/domain/model/CategoryEnum.kt
+++ app/src/main/java/com/example/lumaka/domain/model/CategoryEnum.kt
@@ -7,8 +7,8 @@ enum class CategoryEnum (val id: Int, @StringRes val title: Int) {
     ALL (id = 0, title = R.string.category_all),
     GENERAL (id = 1, title = R.string.category_general),
     WORK (id = 2, title = R.string.category_work),
-    SCHOOL (id = 3, title = R.string.category_school),
-    HOUSEHOLD (id = 4, title = R.string.category_household),
-    GYM (id = 5, title = R.string.category_gym)
-
+    HOUSEHOLD (id = 3, title = R.string.category_household),
+    SCHOOL (id = 4, title = R.string.category_school),
+    SELFCARE (id = 5, title = R.string.category_selfcare),
+    GYM (id = 6, title = R.string.category_gym),
 }
