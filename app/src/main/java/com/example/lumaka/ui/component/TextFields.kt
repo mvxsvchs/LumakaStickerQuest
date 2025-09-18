bf6addce6d79777b14f10d55184babee7620ed16
Maximilian Sachs <maximilian.lukas.sachs@outlook.de>
2025-09-18 18:52:37 +0200

    Fix TextFields focus indicator

--- app/src/main/java/com/example/lumaka/ui/component/TextFields.kt
+++ app/src/main/java/com/example/lumaka/ui/component/TextFields.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
@@ -48,6 +49,8 @@ fun TextInputField(
         colors = TextFieldDefaults.colors(
             unfocusedContainerColor = MaterialTheme.colorScheme.surface,
             focusedContainerColor = MaterialTheme.colorScheme.surface,
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
         ),
         onValueChange = {
             textState.value = it
