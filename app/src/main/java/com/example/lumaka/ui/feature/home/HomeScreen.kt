dae97cc107c7a40f7a5c60e546dc0326cd5e30d0
Maximilian Sachs <maximilian.lukas.sachs@outlook.de>
2025-09-18 19:31:29 +0200

    Refactored `HomeScreen` and deleted `Buttons.kt`

--- app/src/main/java/com/example/lumaka/ui/feature/home/HomeScreen.kt
+++ app/src/main/java/com/example/lumaka/ui/feature/home/HomeScreen.kt
@@ -11,6 +11,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableIntState
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
@@ -23,7 +25,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.lumaka.ui.component.IconButton
 import com.example.lumaka.ui.theme.*
 import com.example.lumaka.util.rememberPreviewNavController
 import com.example.lumaka.R
@@ -47,125 +48,152 @@ fun HomeScreen(
             val textInputState = remember { mutableStateOf(value = "") }
 
             Column {
-                TopAppBar(
-                    title = {
-                        Text(
-                            modifier = Modifier.fillMaxWidth(),
-                            text = stringResource(id = R.string.topbar_title),
-                            style = MaterialTheme.typography.headlineMedium,
-                            textAlign = TextAlign.Center,
-                        )
-                    },
-                    colors = TopAppBarDefaults.topAppBarColors(
-                        containerColor = MaterialTheme.colorScheme.surface,
-                        titleContentColor = MaterialTheme.colorScheme.onBackground,
-                        actionIconContentColor = MaterialTheme.colorScheme.onBackground
-                    ),
-                )
-                LazyRow(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 8.dp),
-                    horizontalArrangement = Arrangement.spacedBy(space = 6.dp),
-                    contentPadding = PaddingValues(horizontal = 8.dp)
-                ) {
-                    val categories = CategoryEnum.entries.toTypedArray()
-                    items(items = categories) { category ->
-                        val isSelected =
-                            remember(key1 = selectedChipId.intValue) { derivedStateOf { selectedChipId.intValue == category.id } }
-                        CategoryChip(
-                            modifier = Modifier
-                                .height(height = 40.dp),
-                            selected = isSelected.value,
-                            title = category.title,
-                            onClick = { selectedChipId.intValue = category.id })
-                    }
-                }
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 8.dp, end = 8.dp, top = 10.dp),
-                    horizontalArrangement = Arrangement.spacedBy(space = 8.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    val expanded = remember { mutableStateOf(value = false) }
-
-                    Box {
-                        Row(
-                            modifier = Modifier
-                                .clickable { expanded.value = true }
-                                .background(
-                                    color = MaterialTheme.colorScheme.surface,
-                                    shape = RoundedCornerShape(size = 8.dp),
-                                )
-                                .width(
-                                    width = 124.dp
-                                )
-                                .height(
-                                    height = 64.dp
-                                ),
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.SpaceBetween,
-                        ) {
-                            Text(
-                                modifier = Modifier.padding(start = 8.dp),
-                                style = MaterialTheme.typography.labelMedium,
-                                fontSize = 14.sp,
-                                text = stringResource(
-                                    id = CategoryEnum.entries.find { it.id == selectedDropdownId.intValue }?.title
-                                        ?: R.string.category_all
-                                )
-                            )
-                            Icon(
-                                modifier = Modifier.padding(end = 8.dp),
-                                imageVector = Icons.Default.ArrowDropDown,
-                                contentDescription = "Dropdown Icon"
-                            )
-                        }
-
-                        SelectionDropdownMenu(
-                            modifier = Modifier,
-                            expanded = expanded.value,
-                            selectedCategoryId = selectedDropdownId.intValue,
-                            onDismiss = { expanded.value = false },
-                            onItemClick = { categoryId ->
-                                selectedDropdownId.intValue = categoryId
-                                expanded.value = false
-                            }
-                        )
-                    }
-
-                    TextInputField(
-                        modifier = Modifier
-                            .height(height = 64.dp)
-                            .weight(weight = 1f),
-                        currentText = textInputState.value,
-                        onTextChange = { textInputState.value = it },
-                    )
-                }
+                TopBarText()
+                ChipsSelection(selectedChipId = selectedChipId)
+                InputRow(textInputState = textInputState, selectedDropdownId = selectedDropdownId)
             }
         },
         bottomBar = {
             NavigationBar(navController = navController)
         }
     ) { padding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(padding)
-                .padding(24.dp),
-            verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.CenterVertically),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+        EmptyState(padding = padding)
+    }
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun TopBarText() {
+    TopAppBar(
+        title = {
             Text(
-                text = stringResource(id = R.string.home_no_tasks),
+                modifier = Modifier.fillMaxWidth(),
+                text = stringResource(id = R.string.topbar_title),
                 style = MaterialTheme.typography.headlineMedium,
-                color = MaterialTheme.colorScheme.onBackground
+                textAlign = TextAlign.Center,
             )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+            titleContentColor = MaterialTheme.colorScheme.onBackground,
+            actionIconContentColor = MaterialTheme.colorScheme.onBackground
+        ),
+    )
+}
+
+@Composable
+private fun ChipsSelection(selectedChipId: MutableIntState) {
+    LazyRow(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 8.dp),
+        horizontalArrangement = Arrangement.spacedBy(space = 6.dp),
+        contentPadding = PaddingValues(horizontal = 8.dp)
+    ) {
+        val categories = CategoryEnum.entries.toTypedArray()
+        items(items = categories) { category ->
+            val isSelected =
+                remember(key1 = selectedChipId.intValue) { derivedStateOf { selectedChipId.intValue == category.id } }
+            CategoryChip(
+                modifier = Modifier
+                    .height(height = 40.dp),
+                selected = isSelected.value,
+                title = category.title,
+                onClick = { selectedChipId.intValue = category.id })
         }
     }
 }
+
+@Composable
+private fun InputRow(textInputState: MutableState<String>, selectedDropdownId: MutableIntState) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp, top = 10.dp),
+        horizontalArrangement = Arrangement.spacedBy(space = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        DropdownSelection(selectedDropdownId = selectedDropdownId)
+
+        TextInputField(
+            modifier = Modifier
+                .height(height = 64.dp)
+                .weight(weight = 1f),
+            currentText = textInputState.value,
+            onTextChange = { textInputState.value = it },
+        )
+    }
+}
+
+@Composable
+private fun DropdownSelection(selectedDropdownId: MutableIntState) {
+    val expanded = remember { mutableStateOf(value = false) }
+
+    Box {
+        Row(
+            modifier = Modifier
+                .clickable { expanded.value = true }
+                .background(
+                    color = MaterialTheme.colorScheme.surface,
+                    shape = RoundedCornerShape(size = 8.dp),
+                )
+                .width(
+                    width = 124.dp
+                )
+                .height(
+                    height = 64.dp
+                ),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Text(
+                modifier = Modifier.padding(start = 8.dp),
+                style = MaterialTheme.typography.labelMedium,
+                fontSize = 14.sp,
+                text = stringResource(
+                    id = CategoryEnum.entries.find { it.id == selectedDropdownId.intValue }?.title
+                        ?: R.string.category_all
+                )
+            )
+            Icon(
+                modifier = Modifier.padding(end = 8.dp),
+                imageVector = Icons.Default.ArrowDropDown,
+                contentDescription = "Dropdown Icon"
+            )
+        }
+
+        SelectionDropdownMenu(
+            modifier = Modifier,
+            expanded = expanded.value,
+            selectedCategoryId = selectedDropdownId.intValue,
+            onDismiss = { expanded.value = false },
+            onItemClick = { categoryId ->
+                selectedDropdownId.intValue = categoryId
+                expanded.value = false
+            }
+        )
+    }
+}
+
+@Composable
+private fun EmptyState(padding: PaddingValues) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(padding)
+            .padding(24.dp),
+        verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = stringResource(id = R.string.home_no_tasks),
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.onBackground
+        )
+    }
+}
+
 
 @Preview(name = "Home Light", showBackground = true)
 @Composable
